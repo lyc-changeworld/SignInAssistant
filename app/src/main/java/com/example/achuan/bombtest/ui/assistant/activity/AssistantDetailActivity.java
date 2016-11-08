534285209ef84815442e453b40203514fc20f3e4
lyc-changeworld <achuan9301@gmail.com>
2016-11-08 22:51:07 +0800

    为签到列表添加字母索引功能

---
+++ app/src/main/java/com/example/achuan/bombtest/ui/assistant/activity/AssistantDetailActivity.java
@@ -0,0 +1,33 @@
+package com.example.achuan.bombtest.ui.assistant.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.example.achuan.bombtest.R;
+import com.example.achuan.bombtest.base.SimpleActivity;
+
+/**
+ * Created by achuan on 16-11-7.
+ * 功能：点击item后打开的活动界面
+ */
+public class AssistantDetailActivity extends SimpleActivity {
+
+
+
+
+    @Override
+    protected int getLayout() {
+        return R.layout.activity_assistant_detail;
+    }
+    @Override
+    protected void initEventAndData() {
+
+
+    }
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+}
